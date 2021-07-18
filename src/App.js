d30af7c08a23398a70e7aa70f4d3db1e0e908a77
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-19 00:25:29 +0800

    updated App.js file and imported all components into it. Layout and functions TBC.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import "./App.css";
-import ReactPortfolio from "./components/ReactPortfolio";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+
 
 function App() {
 	return (
 		<div className="react-portfolio">
-			<ReactPortfolio />
+      <Nav />
+      <main>
+        <About />
+        <Contact />
+        <Portfolio />
+        <Resume />
+      </main>
+      <Footer />
 		</div>
 	);
 }
