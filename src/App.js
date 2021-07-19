2dcca186b2aaabf29d4a98c6fac5c08773609492
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-19 17:25:25 +0800

    added react router and changing app into SPA, added Welcome page as Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
+import Welcome from "./components/Welcome";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
@@ -8,18 +10,22 @@ import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 import "./assets/styles/custom-styles.css";
 
-
 function App() {
 	return (
 		<div className="react-portfolio">
-      <Nav />
-      <main>
-        <About />
-        <Contact />
-        <Portfolio />
-        <Resume />
-      </main>
-      <Footer />
+			<Nav />
+			<Welcome />
+			<main>
+				<Router>
+					<Switch>
+						<Route exact path="/about" component={About} />
+						<Route exact path="/portfolio" component={Portfolio} />
+						<Route exact path="/resume" component={Resume} />
+						<Route exact path="/contact" component={Contact} />
+					</Switch>
+				</Router>
+			</main>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../assets/styles/welcome.css";
+import "../assets/styles/custom-styles.css";
+
+
+function Welcome() {
+    return (
+        <section>
+            Welcome!
+        </section>
+    )
+}
+
+export default Welcome;
