9bbded6004fc0a9a94c8b7d08319e0b981e91e45
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-22 15:58:44 +0800

    updated contact page contents (removed credit section and placed it in footer, adjusted styling for email button)

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "../../assets/styles/contact.css";
 import "../../assets/styles/custom-styles.css";
 import ContactImg from "../../assets/images/contact-me.svg";
-import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
 	return (
@@ -17,9 +16,6 @@ function Contact() {
                 <div class="contact-content">
                     <p>Keen on working together? Have any questions? Feel free to send an email, and I will respond as soon as I can.</p>
                     <a href="mailto:auyoong.aaron@gmail.com" class="contact-btn">E M A I L</a>
-                </div>
-                <div class="credits">
-                    <p>Aaron Au Yoong Copyright (c) 2021-Present. All rights reserved. </p>
                 </div>
             </div>         
 		</section>
