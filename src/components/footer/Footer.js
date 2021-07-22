259f1a8b6d32902eb50bda35d8890e58020d420e
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-22 18:27:31 +0800

    updated href links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -17,8 +17,8 @@ function Footer() {
                     <p>Aaron Au Yoong Copyright (c) 2021-Present. All rights reserved. </p>
                 </div>
                 <div class="icon-links">
-                    <a href="github.com/aaronauyoong"><i class="fab fa-github"></i></a>
-                    <a href="linkedin.com/aaronauyoong"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="www.github.com/aaronauyoong"><i class="fab fa-github"></i></a>
+                    <a href="www.linkedin.com/in/aaron-au-yoong-a69213142/"><i class="fab fa-linkedin-in"></i></a>
                     <a href="mailto:auyoong.aaron@gmail.com"><i class="fas fa-envelope-square"></i></a>
                 </div>
             </div>         
