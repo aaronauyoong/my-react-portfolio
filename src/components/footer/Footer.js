945c630db5f3439e1191bed93695e9fd34187b65
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-22 19:14:19 +0800

    added https:// to external links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,8 +10,8 @@ function Footer() {
                     <p>Aaron Au Yoong Copyright (c) 2021-Present. All rights reserved. </p>
                 </div>
                 <div class="icon-links">
-                    <a href="www.github.com/aaronauyoong"><i class="fab fa-github"></i></a>
-                    <a href="www.linkedin.com/in/aaron-au-yoong-a69213142/"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.github.com/aaronauyoong"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/aaron-au-yoong-a69213142/"><i class="fab fa-linkedin-in"></i></a>
                     <a href="mailto:auyoong.aaron@gmail.com"><i class="fas fa-envelope-square"></i></a>
                 </div>
             </div>         
