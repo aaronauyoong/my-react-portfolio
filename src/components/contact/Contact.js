e34c3cc6fd647facdc945bd40f1fb20bf70f0a95
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-22 15:28:18 +0800

    updated contact page - added image, added button to allow user to email me, adjusting styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 import "../../assets/styles/contact.css";
 import "../../assets/styles/custom-styles.css";
+import ContactImg from "../../assets/images/contact-me.svg";
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-    return (
-        <section>
-            This is the Contact Me Section.
-        </section>
-    )
+	return (
+		<section>
+            <div class="contact">
+                <div class="contact-header">
+                    <h2>LET'S CONNECT!</h2>    
+                </div>
+                <div class="contact-img">
+                    <img src={ContactImg} alt="Contact me cartoon" />
+                </div>
+                <div class="contact-content">
+                    <p>Keen on working together? Have any questions? Feel free to send an email, and I will respond as soon as I can.</p>
+                    <a href="mailto:auyoong.aaron@gmail.com" class="contact-btn">E M A I L</a>
+                </div>
+                <div class="credits">
+                    <p>Aaron Au Yoong Copyright (c) 2021-Present. All rights reserved. </p>
+                </div>
+            </div>         
+		</section>
+	);
 }
 
 export default Contact;
