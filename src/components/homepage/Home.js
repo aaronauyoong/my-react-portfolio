73ab801e7dd60ad05091e5a562a23d7f3bfb61a7
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-07 00:21:19 +0800

    added updated logo to navbar. Updated navbar styling and landing page styling, removed image and switched typewriter effect to hello world text. updated landing page text.

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,33 +1,31 @@
-import React from "react";
-import "../../assets/styles/custom-styles.css";
-import HomepageWebDev from "../../assets/images/homepage-webdev.svg";
+import React from 'react';
+import '../../assets/styles/custom-styles.css';
+import HomepageWebDev from '../../assets/images/homepage-webdev.svg';
 // import Typewriter from "typewriter-effect";
 
 function Home() {
-	return (
-		<section class="landing-page">
-			<div>
-				<div class="hello-world">
-					<h2> &lt; Hello World! /&gt; </h2>
-				</div>
-				<div class="home-myself">
-					<h2 class="myself-text">
-						I'm Aaron Au Yoong,
-						<div class="typewriter">
-							<h2 class="my-job"> &gt;Web Developer.</h2>
-						</div>
-						
-					</h2>
-					<div class="home-image">
-						<img
-							src={HomepageWebDev}
-							alt="This is a Web Dev cartoon from unDraw"
-						></img>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section class="landing-page">
+            <div>
+                {/* <div class="home-image">
+                    <img
+                        src={HomepageWebDev}
+                        alt="This is a Web Dev cartoon from unDraw"
+                    ></img>
+                </div> */}
+                <div class="hello-world">
+                    <div class="typewriter">
+                        <h4> &lt; Hello World! /&gt;</h4>
+                    </div>
+                </div>
+                <div class="home-myself">
+                    <p class="my-name">My name is</p>
+                    <h2 class="myself-text">Aaron Au Yoong.</h2>
+                    <h3 class="create-text">I create things for the web.</h3>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Home;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import "../../assets/styles/nav.css";
-import "../../assets/styles/custom-styles.css";
-
+import '../../assets/styles/nav.css';
+import '../../assets/styles/custom-styles.css';
+import Logo from '../../assets/images/website-logo.png';
 
 function Nav() {
     return (
-        <header >
+        <header>
             <nav class="nav-bar">
-                <a href="/" class="nav-btn">Home</a>
-                <a href="/about" class="nav-btn">About</a>
-                <a href="/portfolio" class="nav-btn">Portfolio</a>
-                <a href="/contact" class="nav-btn">Contact</a>
-                <a href="/resume" class="nav-btn">Resume</a>
+                <div class="square logo">
+                    <img src={Logo} alt="This is the website logo." class="lav-border"/>
+                </div>
+                <a href="/" class="nav-btn">
+                    Home
+                </a>
+                <a href="/about" class="nav-btn">
+                    About
+                </a>
+                <a href="/portfolio" class="nav-btn">
+                    Portfolio
+                </a>
+                <a href="/contact" class="nav-btn">
+                    Contact
+                </a>
+                <a href="/resume" class="nav-btn">
+                    Resume
+                </a>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Nav;
