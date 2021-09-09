f9194eca1e2de1e303a6bd9ff52d0f1f00fa33b2
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-09 16:55:00 +0800

    restyled about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,51 +1,51 @@
-import React from "react";
-import "../../assets/styles/about.css";
-import "../../assets/styles/custom-styles.css";
-import MyPicture from "../../assets/images/my-picture.jpg"
+import React from 'react';
+import '../../assets/styles/about.css';
+import '../../assets/styles/custom-styles.css';
+import MyPicture from '../../assets/images/my-picture.jpg';
 
 function About() {
-	return (
-		<section>
-			<div className="about-me-container">
-				<div className="about-me-image">
-					<img src={MyPicture} alt="Portrait of Aaron Au Yoong"/>
-				</div>
-				<div className="about-me">
-					<h2><span class="title-number">01.</span> About Me</h2>
-				</div>
-				<div className="main-paragraph">
-					<p>
-						My name is Aaron Au Yoong, and I am starting my journey as a website
-						and software developer. After various experiences over the past 2
-						years, I have emerged as a fresh web developer after earning a
-						certificate in full-stack web development from Monash University. I
-						have developed technical skills and languages that cover front-end
-						and back-end development, which include JavaScript, Node JS, React
-						JS, MongoDB and SQL. 
+    return (
+        <section>
+            <div className="about-me-container">
+                <div className="about-me">
+                    <h2>
+                        <span class="title-number">01.</span> About Me
+                    </h2>
+                </div>
+                <div className="about-me-image">
+                    <img src={MyPicture} alt="Portrait of Aaron Au Yoong" />
+                </div>
 
-						<br/>
-						<br/>
-						
-						I am a keen learner and am looking to work with
-						fellow developers to build impactful technologies. As a Master’s
-						graduate with a year of full-time experience in the IT industry, I
-						have developed soft skills that include critical thinking,
-						communication and teamwork. 
-
-						<br/>
-						<br/>
-						
-						I aim to further understand people,
-						relationships, interactions and the technological applications that
-						can be used to enhance work-life satisfaction, efficiency and
-						productivity through the combination of studying a Bachelor of
-						Science (Psych), Masters of Business Information Technology and a
-						Full Stack Web Development Diploma.
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+                <div className="about-me-content">
+                    <p>
+                        My name is Aaron Au Yoong, and I am starting my journey
+                        as a website and software developer. After various
+                        experiences over the past 2 years, I have emerged as a
+                        fresh web developer after earning a certificate in
+                        full-stack web development from Monash University. I
+                        have developed technical skills and languages that cover
+                        front-end and back-end development, which include
+                        JavaScript, Node JS, React JS, MongoDB and SQL.
+                        <br />
+                        <br />
+                        I am a keen learner and am looking to work with fellow
+                        developers to build impactful technologies. As a
+                        Master’s graduate with a year of full-time experience in
+                        the IT industry, I have developed soft skills that
+                        include critical thinking, communication and teamwork.
+                        <br />
+                        <br />I aim to further understand people, relationships,
+                        interactions and the technological applications that can
+                        be used to enhance work-life satisfaction, efficiency
+                        and productivity through the combination of studying a
+                        Bachelor of Science (Psych), Masters of Business
+                        Information Technology and a Full Stack Web Development
+                        Diploma.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default About;
