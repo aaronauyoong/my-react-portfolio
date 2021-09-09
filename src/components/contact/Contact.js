bd7323e2eb864936f8dfdb267109413c024b88c4
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-09 16:35:11 +0800

    added semantic markup for contact page. adjusted styling and made page more mobile friendly

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,25 +1,35 @@
-import React from "react";
-import "../../assets/styles/contact.css";
-import "../../assets/styles/custom-styles.css";
-import ContactImg from "../../assets/images/contact-me.svg";
+import React from 'react';
+import '../../assets/styles/contact.css';
+import '../../assets/styles/custom-styles.css';
+import ContactImg from '../../assets/images/contact-me.svg';
 
 function Contact() {
-	return (
-		<section>
-            <div className="contact">
-                <div className="contact-header">
-                    <h2><span className="title-number">03.</span> Contact</h2>    
-                </div>
+    return (
+        <section className="contact">
+            <div className="contact-header">
+                <h2>
+                    <span className="title-number">03.</span> Contact
+                </h2>
+            </div>
+            <div className="contact-content">
                 <div className="contact-img">
                     <img src={ContactImg} alt="Contact me cartoon" />
                 </div>
-                <div className="contact-content">
-                    <p>Keen on working together? Have any questions? Feel free to send an email, and I will respond as soon as I can.</p>
-                    <a href="mailto:auyoong.aaron@gmail.com" class="contact-btn">E M A I L</a>
+                <div className="contact-summary">
+                    <p>
+                        Keen on working together? Have any questions? Feel free
+                        to send an email, and I will respond as soon as I can.
+                    </p>
+                    <a
+                        href="mailto:auyoong.aaron@gmail.com"
+                        class="contact-btn"
+                    >
+                        Say Hello
+                    </a>
                 </div>
-            </div>         
-		</section>
-	);
+            </div>
+        </section>
+    );
 }
 
 export default Contact;
