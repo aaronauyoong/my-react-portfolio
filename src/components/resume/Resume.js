377e93d858d9abbcd5eb4c071b84cd4dd3f4732d
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-09 16:05:06 +0800

    updated resume component - styling and contents. shifted download resume from clicking on image to clicking on button. Condensed content of page. Made it more mobile friendly.

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,62 +1,46 @@
-import React from "react";
-import "../../assets/styles/resume.css";
-import "../../assets/styles/custom-styles.css";
-import ResumeCartoon from "../../assets/images/undraw-resume.svg";
-import MyResume from "../../assets/pdf/my-resume.pdf";
+import React from 'react';
+import '../../assets/styles/resume.css';
+import '../../assets/styles/custom-styles.css';
+import ResumeCartoon from '../../assets/images/undraw-resume.svg';
+import MyResume from '../../assets/pdf/my-resume.pdf';
 
 function Resume() {
-	return (
-		<section>
-			<div className="resume">
-				<div className="resume-header">
-					<h2><span className="title-number">04.</span> Resume</h2>
-				</div>
-				<div className="resume-img">
-					<a href={MyResume}>
-						<img src={ResumeCartoon} alt="Contact me cartoon" />
-					</a>
-				</div>
-				<div className="resume-content row">
-					<div className="resume-section col-lg-6">
-						<h3>Here are some technologies I've worked with recently:</h3>
-						<ul className="unstyled-bullets">
-							<li>JavaScript (ES6+)</li>
-							<li>MERN Stack (MongoDB, Express.js, React.js, Node.js)</li>
-							<li>Bootstrap</li>
-							<li>Handlebars</li>
-							<li>MySQL</li>
-						</ul>
-					</div>
-					<div className="resume-section col-lg-6">
-						<h3>Trained in Agile Development:</h3>
-						<ul className="unstyled-bullets">
-							<li>Kanban, Project Management</li>
-							<li>Issues</li>
-							<li>User Stories</li>
-							<li>Sprints</li>
-							<li>Standup</li>
-						</ul>
-					</div>
-					<div className="resume-section col-lg-6">
-						<h3>Familiar With Deployment Platforms:</h3>
-						<ul className="unstyled-bullets">
-							<li>Heroku</li>
-							<li>MongoAtlas</li>
-							<li>Netlify</li>
-							<li>Git</li>
-						</ul>
-					</div>
-					<div className="resume-section col-lg-6">
-						<h3>Familiar With Popular MVC Frameworks:</h3>
-						<ul class="unstyled-bullets">
-							<li>Django</li>
-							<li>Laravel</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section className="resume">
+            <div className="resume-header">
+                <h2>
+                    <span className="title-number">04.</span> Resume
+                </h2>
+            </div>
+            <div className="resume-content">
+                <div className="resume-img">
+                        <img src={ResumeCartoon} alt="Contact me cartoon" />
+                </div>
+                <div className="resume-summary">
+                    <div className="resume-section">
+                        <h3>
+                            Here are some of the technologies I've worked with
+                            recently:
+                        </h3>
+                        <ul className="unstyled-bullets">
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript (ES6+)</li>
+                            <li>
+                                MERN Stack (MongoDB, Express.js, React.js,
+                                Node.js)
+                            </li>
+                            <li>Bootstrap</li>
+                            <li>Handlebars</li>
+                            <li>GraphQL</li>
+                            <li>MySQL</li>
+                        </ul>
+						<a href={MyResume} className="download-resume">Click here to download my resume!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Resume;
