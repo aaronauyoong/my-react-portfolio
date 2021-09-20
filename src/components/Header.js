240c6a58fe5f647342b5224900c22dd6a36125fa
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-20 20:34:19 +0800

    finished mobile friendly navbar (with hamburger icon). styling for pop up navigation completed.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,18 @@ import Logo from '../assets/images/website-logo.png';
 function Header() {
     return (
         <header className="nav-bar">
-            <div className="square logo">
-                <a href="/">
-                    <img
-                        src={Logo}
-                        alt="This is the website logo."
-                        className="lav-border"
-                    />
-                </a>
+            <div className="logo-container">
+                <div className="square logo">
+                    <a href="/">
+                        <img
+                            src={Logo}
+                            alt="This is the website logo."
+                            className="lav-border"
+                        />
+                    </a>
+                </div>
             </div>
+
             <Navigation />
         </header>
         // <header className="border-b p-3 flex justify-between items-center">
--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../../assets/styles/nav.css';
 import '../../assets/styles/custom-styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false);
@@ -11,7 +11,13 @@ function Navigation() {
 
     if (showMenu) {
         menu = (
-            <div className="fixed top-0 left-0 w-4/5 h-full z-50 shadow">
+            <div
+                className="menu-bar animated fadeIn shadow"
+                style={{
+                    marginRight: '10px',
+                    borderRadius: '10px'
+                }}
+            >
                 <a href="/about" className="nav-btn hover-underline-animation">
                     01. About
                 </a>
@@ -36,13 +42,16 @@ function Navigation() {
 
     return (
         <nav>
+            {menu}
             <span className="text-xl">
                 <FontAwesomeIcon
+                    className="menu-icon"
                     icon={faBars}
                     onClick={() => setShowMenu(!showMenu)}
-                />
+                >
+                    <i className={showMenu ? { faTimes } : { faBars }} />
+                </FontAwesomeIcon>
             </span>
-            { menu }
         </nav>
     );
 }
