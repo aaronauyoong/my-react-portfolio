2d6214b3b03b04f7351109fb15abea815e6c0ad5
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-09-20 17:15:09 +0800

    commented out unused code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from './nav/Nav';
+// import Nav from './nav/Nav';
 import Navigation from './nav/Navigation';
 import Logo from '../assets/images/website-logo.png';
 
